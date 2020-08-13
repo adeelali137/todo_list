3bc487b565bc339a8bc2ca69faa767650c7d0e12
Adeel Ismail Shaikh <64972500+adeelali137@users.noreply.github.com>
2020-08-13 15:17:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+// Calling Elements
+var inputbtn = document.getElementById('inputbtn');
+var additem = document.getElementById('additem');
+var dltitem = document.getElementById('dltitem');
+var text = document.getElementById('text');
+
+
+// Adding Items
+function additems(){
+if(inputbtn.value === '' || inputbtn.value === " "){
+    alert("you must write")
+}else{
+    var para = document.createElement('p');
+    text.append(para);
+    para.append(inputbtn.value)
+}
+
+// Add delet btn in each text node
+var btn = document.createElement("button");
+btn.innerHTML = "Delete";
+para.appendChild(btn);
+btn.style.padding = '4px';
+btn.style.float = 'right';
+btn.setAttribute("onclick" , "dlteachitem(this)")
+
+// Add done btn in each text node
+var btn = document.createElement("button");
+btn.innerHTML = "Done";
+para.appendChild(btn);
+btn.style.padding = '4px';
+btn.style.float = 'right';
+btn.style.marginRight = '10px';
+btn.setAttribute("onclick" , "done(this)")
+
+// empty input after hit add
+inputbtn.value = "";
+}
+
+function done(e){
+    e.parentNode.classList.toggle("checked")
+}
+
+function dlteachitem(e){
+    e.parentNode.remove();
+}
+
+// Deleting All Items
+function deletitems(){
+    text.innerHTML = " ";
+}
